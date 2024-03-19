1c53e99d7cc67fe9c91d07354941c6faf22bad61
Prabeshika <prabeshika14@gmail.com>
2024-03-19 18:19:41 +0100

    new additions

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
-import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "axios";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [state, setState] = useState([]);
-
-  useEffect(
-    () => async () => {
-      const response = await axios.get("https://fakestoreapi.com/products");
-      console.log(response);
-      setState(response.data);
-    },
-    []
-  );
   return (
     <div className="App relative z-20 pt-20">
-      <h1>fakeStore</h1>
-      {state.map((ele) => {
-        return (
-          <>
-            <li>{ele.id}</li>
-            <li>{ele.title}</li>
-            <li>{ele.name}</li>
-            <img src={ele.image} />
-          </>
-        );
-      })}
       <Navbar />
 
       <Routes>
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+const apiURL = "https://fakestoreapi.com/products";
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
+  // The state variable `products` is currently an empty array [],
   // but you should use it to store the response from the Fake Store API (the list of products).
+
   const [products, setProducts] = useState([]);
 
   // To fetch the list of products, set up an effect with the `useEffect` hook:
+  const getAllProducts = () => {
+    axios
+      .get(apiURL)
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  };
 
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
   return (
     <div className="ProductListPage">
-      {/* Render list of products here */}
+      {products.map((product) => {
+        return (
+          <div className="card" key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <h3>{product.title}</h3>
+              <img src={product.image} />
+              <p>${product.price}</p>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
